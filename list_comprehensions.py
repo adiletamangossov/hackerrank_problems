31a20c2814b1a571adb89e0e383a0813d3e0213f
adilet <94785756+adiletamangossov@users.noreply.github.com>
2024-03-30 12:15:33 +0500

    Update list_comprehensions.py

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,1 +1,8 @@
-
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    result = []
+    [[[result.append([num1, num2, num3]) for num3 in range(z+1) if num1 + num2 + num3 != n] for num2 in range(y+1)] for num1 in range(x+1)]
+    print(result)
