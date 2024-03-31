605ff2d2aaa82adf1e6d2182a415b0815096cd85
cyxf <cyxf@mail>
2024-03-31 11:56:02 +0500

    updated code: removed whitespaces; added if main

--- solutions/polar_coordinates.py
+++ solutions/polar_coordinates.py
@@ -6,6 +6,7 @@ def polar_coordinates(z):
     return r, fi
 
 
-z = complex(input())
-for num in polar_coordinates(z):
-    print(num)
+if __name__ == '__main__':
+    z = complex(input())
+    for num in polar_coordinates(z):
+        print(num)
